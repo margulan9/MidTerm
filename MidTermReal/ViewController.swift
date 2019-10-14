0889f6702225a2683a4f0f27629cd054253b413a
margulan9 <sugirbaymargulan@gmail.com>
2019-10-14 13:57:03 +0600

    Initial Commit

---
+++ MidTermReal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MidTermReal
+//
+//  Created by Margulan Sugirbay on 10/14/19.
+//  Copyright © 2019 Margulan Sugirbay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
